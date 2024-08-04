fd4bfedd92b9add17ad38a9635f55166ce84c308
agungsaputraprojects <agungsaputra.projects@gmail.com>
2024-08-05 01:07:39 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("@node-rs/bcrypt");
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mfwesanxtivtlgkmpwbp.supabase.co",
+      },
+    ],
+    unoptimized: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
