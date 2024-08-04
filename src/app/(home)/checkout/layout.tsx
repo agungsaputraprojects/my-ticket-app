9cd0e9b7084746b1ac5a21ec4802c5ff0c603c28
agungsaputraprojects <agungsaputra.projects@gmail.com>
2024-08-05 02:13:26 +0700

    first commit

--- src/app/(home)/checkout/layout.tsx
+++ src/app/(home)/checkout/layout.tsx
@@ -5,23 +5,23 @@ import QCProvider from "../available-flights/providers/query-provider";
 import Script from "next/script";
 
 interface LayoutProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const midtransClientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY ?? "";
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-	return (
-		<>
-			<QCProvider>{children}</QCProvider>
+  return (
+    <>
+      <QCProvider>{children}</QCProvider>
 
-			<Script
-				type="text/javascript"
-				src="https://app.sandbox.midtrans.com/snap/snap.js"
-				data-client-key={midtransClientKey}
-			/>
-		</>
-	);
+      <Script
+        type="text/javascript"
+        src="https://app.sandbox.midtrans.com/snap/snap.js"
+        data-client-key={midtransClientKey}
+      />
+    </>
+  );
 };
 
 export default Layout;
